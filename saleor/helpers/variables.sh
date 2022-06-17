66fe4b1a920898b3e93171105ffdcdf3eef7fadb
dnilasor <rosalind26@gmail.com>
2022-06-17 10:03:33 -0700

    add allowed hosts to helper file

--- saleor/helpers/variables.sh
+++ saleor/helpers/variables.sh
@@ -2,4 +2,5 @@
 set -e # exit on error
 
 export RSA_PRIVATE_KEY=$(cat /etc/secrets/saleor-key)
+export ALLOWED_HOSTS="['$RENDER_EXTERNAL_HOSTNAME']"
 export ALLOWED_CLIENT_HOSTS="${[$STOREFRONT_URL, $DASHBOARD_URL]}"
